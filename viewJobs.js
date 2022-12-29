f0b25e7343ef2274744be5a99676680f8d90a2a4
VictorSocolovschi <victorandhadar@gmail.com>
2022-12-29 17:46:24 +0200

    add button and beautify cards on viewjobs

--- viewJobs.js
+++ viewJobs.js
@@ -25,21 +25,23 @@ import {
 
       querySnapshot.forEach((doc) => {
         const job = doc.data();
-       
         jobsContainer.innerHTML += `
     <div class="card card-body mt-2 border-primary">
-      <h3 class="h5">${job.title}</h3>
-      <label for="description">: תיאור התפקיד</label>
-      <p>${job.description}</p>
-      <label for="description">: מיקום</label>
-      <p>${job.location}</p>  
-      <label for="description">: היקף המשרה</label>
-      <p>${job.scope}</p> 
-      <label for="description">: דרישות</label>
-      <p>${job.standarts}</p>  
+      <h2 class="h5">${job.title}</h2>
+      <label for="description" class="job-label"><strong>: תיאור התפקיד</strong></label>
+      <p class="job-description">${job.description}</p>
+      <label for="description" class="job-label"><strong>: מיקום</strong></label>
+      <p class="job-location">${job.location}</p> 
+      <label for="description" class="job-label"><strong>: היקף המשרה</strong></label>
+      <p class="job-scope">${job.scope}</p>  
+      <label for="description" class="job-label"><strong>: דרישות</strong></label>
+      <p class="job-requirements">${job.standarts}</p> 
+      <br>
+      <a href="https://www.example.com"><button>צור קשר</button></a>
+
     </div>`;
 
       });
     });
   });
-  
+
