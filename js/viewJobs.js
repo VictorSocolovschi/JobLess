4b5193e059377d5b9064ba50d6ba2f0f54e28a7f
guyhaliva123 <116512226+guyhaliva123@users.noreply.github.com>
2023-01-02 10:38:51 +0200

    change window on view Jobs

--- js/viewJobs.js
+++ js/viewJobs.js
@@ -26,21 +26,37 @@ import {
       querySnapshot.forEach((doc) => {
         const job = doc.data();
         jobsContainer.innerHTML += `
-    <div class="card card-body mt-2 border-light" style="width: 18rem;">
+    
+    <div class="card card-body mt-2 border-light" style="max-width: 450px;
+      max-height: 300px; overflow: scroll; position: relative;">
       <h2 class="h5">${job.title}</h2>
-      <label for="description" class="job-label"><strong>: תיאור התפקיד</strong></label>
+
+      <label for="description" class="job-label">
+      <strong>: תיאור התפקיד</strong></label>
+      
       <p class="job-description">${job.description}</p>
-      <label for="description" class="job-label"><strong>: מיקום</strong></label>
+
+      <label for="description" class="job-label">
+      <strong>: מיקום</strong></label>
+
       <p class="job-location">${job.location}</p> 
-      <label for="description" class="job-label"><strong>: היקף המשרה</strong></label>
+      
+      <label for="description" class="job-label">
+      <strong>: היקף המשרה</strong></label>
+
       <p class="job-scope">${job.scope}</p>  
-      <label for="description" class="job-label"><strong>: דרישות</strong></label>
+
+      <label for="description" class="job-label">
+      <strong>: דרישות</strong></label>
+
       <p class="job-requirements">${job.standarts}</p> 
+      
       <br>
+
       <a href="mailto:${job.pubmail}"><button-contact>צור קשר</button></a>
       
-
-    </div>`;
+    </div>
+    `;
 
       });
     });
