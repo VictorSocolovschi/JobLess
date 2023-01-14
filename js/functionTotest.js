c20b92666aa44ebf256d0bcdb1bd006fc0f4d978
Guy aloosh <projectscejobless@gmail.com>
2023-01-14 14:35:39 +0200

    unit test

--- js/functionTotest.js
+++ js/functionTotest.js
@@ -3,4 +3,3 @@ export function convertToLowercase(str) {
     return str.toLowerCase();
   }
 
-
--- js/functionTotest.test.js
+++ js/functionTotest.test.js
@@ -15,4 +15,27 @@ jest.mock('./functionTotest.js', () => {
       expect(functionTotest.convertToLowerCase("HELLO")).toEqual("hello");
       expect(functionTotest.convertToLowerCase("WorlD")).toEqual("world");
     });
+    test('convertToLowerCase - empty string input', () => {
+      const functionTotest = require('./functionTotest');
+      functionTotest.convertToLowerCase.mockImplementation(string => string.toLowerCase());
+      expect(functionTotest.convertToLowerCase("")).toEqual("");
+    });
+        
+    test('convertToLowerCase - numeric input', () => {
+      const functionTotest = require('./functionTotest');
+      functionTotest.convertToLowerCase.mockImplementation(string => string.toLowerCase());
+      expect(functionTotest.convertToLowerCase("12345")).toEqual("12345");
+    });
+        
+    test('convertToLowerCase - special characters input', () => {
+      const functionTotest = require('./functionTotest');
+      functionTotest.convertToLowerCase.mockImplementation(string => string.toLowerCase());
+      expect(functionTotest.convertToLowerCase("!@#$%^&")).toEqual("!@#$%^&");
+    });
+        
+    test('convertToLowerCase - already lowercase input', () => {
+      const functionTotest = require('./functionTotest');
+      functionTotest.convertToLowerCase.mockImplementation(string => string.toLowerCase());
+      expect(functionTotest.convertToLowerCase("already lowercase")).toEqual("already lowercase");
+    });
   });
